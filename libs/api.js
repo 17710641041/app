2f80ff2615a717f4605a699c739b07d6c6d05bf1
songguojun <gj_yxcom@163.com>
2020-04-18 15:16:53 +0800

    更新

--- libs/api.js
+++ libs/api.js
@@ -3,21 +3,12 @@ const api = 'http://mayiren.itdiguo.cn';
 
 /**接口配置*/
 const contactInterface = {
-<<<<<<< HEAD
-	  //获取banner图
-	  banner: `${api}/shop/Banner/banner`,
-	  //导航
-	  nav: `${api}/shop/index/nav`,
-	  //分类
-	  classList: `${api}/shop/index/index`,
-=======
-	//获取banner图
-	banner: `${api}/shop/Banner/banner`,
-	//导航
-	nav: `${api}/shop/index/nav`,
-	//分类
-	nav: `${api}/shop/index/nav`,
->>>>>>> 2c4a7fad7b7855eddc81989315b64eadbbc94aec
+  //获取banner图
+  banner: `${api}/shop/Banner/banner`,
+  //导航
+  nav: `${api}/shop/index/nav`,
+  //分类
+  classList: `${api}/shop/index/index`,
 }
 
 export { contactInterface }
